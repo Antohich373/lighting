482ee5e2074e9529ff6b08a25288209d7a7914ce
anton <gluskov352@gmail.com>
2021-07-14 12:46:52 +0300

    dasdsa

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 let navigation = document.querySelector('.header-navigation--close')
-let buttonOpen = document.querySelector('.header-open--nav')
+let buttonOpen = document.querySelector('.header__toggle')
+let modalsShow = document.querySelector('.modal-show')
 
 buttonOpen.addEventListener('click', function(evt){
   evt.preventDefault();
-  navigation.classList.toggle('header-navigation--open')
+  navigation.classList.toggle('header-navigation--open');
+  modalsShow.classList.toggle('modal-show--active');
+  buttonOpen.classList.toggle('header__toggle--opened')
 })
 
 $('[type=tel]').mask('+7 (999) 999-99-99')
